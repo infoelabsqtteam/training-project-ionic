71df0e8bda9755df8efc01938870c83ca4a9ef11
kwebdev11 <kunalwebdeveloper11@gmail.com>
2024-03-21 18:21:57 +0530

    Remove unused code

--- src/app/service/androidpermissions.service.ts
+++ src/app/service/androidpermissions.service.ts
@@ -6,12 +6,9 @@ import { AndroidPermissions } from '@ionic-native/android-permissions/ngx';
 import { App_googleService, NotificationService } from '@core/ionic-core';
 import { AlertController, Platform } from '@ionic/angular';
 import { OpenNativeSettings } from '@ionic-native/open-native-settings/ngx';
-import { Filesystem, Directory, FilesystemDirectory } from '@capacitor/filesystem';
 import { HttpClient } from '@angular/common/http';
 import { DomSanitizer } from '@angular/platform-browser';
 
-import { Capacitor } from '@capacitor/core';
-import { FileOpener } from '@capacitor-community/file-opener';
 
 @Injectable({
   providedIn: 'root'
@@ -25,11 +22,6 @@ export class AndroidpermissionsService {
   options: any;
   inputaddress: any;
   result: any;
-   // Small PDF
-   smallFile = 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf';
-
-   // Large PDF (~25mb)
-   largeFile = 'https://research.nhm.org/pdfs/10840/10840.pdf';
 
   constructor(
     private nativeGeocoder: NativeGeocoder,
@@ -44,15 +36,6 @@ export class AndroidpermissionsService {
     private http: HttpClient, 
     private sanitizer: DomSanitizer
   ) { }
-
-    // requestPermisiions() {
-    //   this.androidPermission.requestPermissions(
-    //     [this.androidPermission.PERMISSION.ACCESS_NETWORK_STATE, this.androidPermission.PERMISSION.READ_EXTERNAL_STORAGE,
-    //     this.androidPermission.PERMISSION.WRITE_EXTERNAL_STORAGE, this.androidPermission.PERMISSION.CAMERA, this.androidPermission.PERMISSION.GET_ACCOUNTS,
-    //     this.androidPermission.PERMISSION.ACCESS_FINE_LOCATION, this.androidPermission.PERMISSION.ACCESS_COARSE_LOCATION,
-    //     this.androidPermission.PERMISSION.RECORD_AUDIO, this.androidPermission.PERMISSION.MODIFY_AUDIO_SETTINGS
-    //     ]);
-    // }
 
   async internetExceptionError(){
       const alert = await this.alertController.create({
@@ -84,73 +67,6 @@ export class AndroidpermissionsService {
     });
     return alert;
   }
-
-  // internetPermission(){
-  //   return  this.androidPermission.checkPermission(this.androidPermission.PERMISSION.INTERNET || this.androidPermission.PERMISSION.ACCESS_WIFI_STATE).then(
-  //     result => {
-  //       if (result.hasPermission) {
-  //         this.enableGPS();
-  //         console.log("Internet Access");
-  //       }else {
-  //         this.androidPermission.requestPermission(this.androidPermission.PERMISSION.INTERNET);
-  //         return true;
-  //       }
-  //     },
-  //     error => {
-  //       console.log(error);
-  //       this.notificationService.showAlert("Your Device Has LOST Internet Access", 'No Internet',['OK'])
-  //     }
-  //   )
-  // }
-
-  // checkPermission() {
-  //   return this.androidPermission.checkPermission(this.androidPermission.PERMISSION.ACCESS_COARSE_LOCATION).then(
-  //      result => {
-  //        if (result.hasPermission) {
-  //          this.enableGPS();
-  //        } else {
-  //          this.locationAccPermission();
-  //        }
-  //      },
-  //      error => {
-  //          // alert("GPS not active. Please Enable GPS.");
-  //          console.log(error);
-  //      }
-  //    );
-  //  }
-  
-  //  locationAccPermission() {
-  //    this.locationAccuracy.canRequest().then((canRequest: boolean) => {
-  //      if (canRequest) {
-  //      } else {
-  //        this.androidPermission.requestPermission(this.androidPermission.PERMISSION.ACCESS_COARSE_LOCATION)
-  //          .then(
-  //            () => {
-  //            return  this.enableGPS();
-  //            },
-  //            error => {
-  //              alert("GPS not active. Please Enable GPS.")
-  //              console.log(error)
-  //            }
-  //          );
-  //      }
-  //    });
-  //  }
-  
-  //  enableGPS() {
-  //    this.locationAccuracy.request(this.locationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY).then(
-  //      () => {
-  //        this.getCurrentLocation();
-  //        return true;
-  //      },
-  //      // error => alert(JSON.stringify(error))
-  //      error => {
-  //          // alert("GPS unable to locate your position. Please check GPS is Active.")
-  //          this.notificationService.showAlert("GPS unable to locate your position.", 'Enable GPS',['OK'])
-  //          console.log(JSON.stringify(error))
-  //      }
-  //    );
-  //  }
   
   async getCurrentLocation(){
     try{
@@ -189,76 +105,5 @@ export class AndroidpermissionsService {
       console.log(settingName + "has been open");
     });
   }
-
-  // Download and Print PDF
-
-  async downloadAndPrintPdf(blobData: any,filename?:string): Promise<void> {
-    try {
-      const newFileName = new Date().toDateString().replace(/:/g, '-') + '.pdf'
-      let data = {
-        msg: "",
-        status: false,
-        fileName: filename ? filename : ''
-      }
-      let fileName = '';      
-      if(filename){
-        filename = fileName
-      }else{
-        fileName = "download-pdf.pdf"
-      }
-      // Check write permission
-      let checkwritepermission = await Filesystem.checkPermissions();
-      if(checkwritepermission){
-
-      }else{
-        checkwritepermission = await Filesystem.requestPermissions();
-      }
-      // Save the blob data as a PDF file
-      const filePath = `${Directory.Documents}/${fileName}`;
-      await Filesystem.writeFile({
-        path: filePath,
-        data: blobData,
-        directory: Directory.Documents
-      });
-      // Write the blob to the app's local filesystem
-        const writeResult = await Filesystem.writeFile({
-          path: fileName,
-          data: blobData,
-          directory: Directory.Documents
-        });
-
-      // Print the PDF file
-      await this.printPdf(filePath);
-    } catch (error) {
-      console.error('Error downloading and printing PDF:', error);
-    }
-  }
-
-  async printPdf(filePath: string): Promise<void> {
-    try {
-      // Print the PDF file using the Capacitor Filesystem plugin
-      if (!filePath) {
-        throw new Error(`Unable to download ${filePath}`);
-      }
-      await FileOpener.open({
-        filePath: filePath,
-        openWithDefault: true
-      });
-
-      await Filesystem.getUri({
-        path: filePath,
-        directory: Directory.Documents
-      });
-      const fileUrl:any = this.sanitizer.bypassSecurityTrustUrl(filePath);
-      window.open(fileUrl, '_blank');
-    } catch (error) {
-      console.error('Error printing PDF:', error);
-    }
-  }
-
-  // Download and Print PDF end
-  /**
-   * This will download a PDF file and open it using @capacitor-community/file-opener
-   */
 
 }
