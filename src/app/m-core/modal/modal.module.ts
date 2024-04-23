835d49729f02189a6b18d2df80a6e2b1534e6e52
kwebdev <87276852+kwebdev11@users.noreply.github.com>
2024-04-23 21:07:34 +0530

    angular-google-chart package removed , revert the angular json file changes, and delete the extra file commited

--- src/app/m-core/modal/modal.module.ts
+++ src/app/m-core/modal/modal.module.ts
@@ -9,7 +9,7 @@ import { IonicModule } from '@ionic/angular';
 import { CallDataRecordFormComponent } from './call-data-record-form/call-data-record-form.component';
 import { ChartFilterComponent } from './chart-filter/chart-filter.component';
 import { NgChartsModule } from 'ng2-charts';
-import { GoogleChartsModule } from 'angular-google-charts';
+// import { GoogleChartsModule } from 'angular-google-charts';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { ModalComponent } from './modal.component';
 import { FileViewsModalComponent } from './file-views-modal/file-views-modal.component';
@@ -34,7 +34,7 @@ const modal =[
     BrowserModule,
     IonicModule.forRoot(),
     NgChartsModule,
-    GoogleChartsModule,
+    // GoogleChartsModule,
     NgSelectModule
   ],
   declarations: modal,
